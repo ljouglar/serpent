651ad44a4f4b03e220e6fae8b39ccfff1c50073b
ljouglar <laurent.jouglar@gmail.com>
2020-10-24 17:52:45 +0200

    Un peu de nettoyage

--- main.py
+++ main.py
@@ -8,34 +8,36 @@ def compute_next_frame():
     global direction
     can.delete("all")
     snake.avance(direction)
-    print(snake)
     snake.show(can)
     tk.after(1000, lambda: compute_next_frame())
 
 
 def right(event):
     global direction
-    direction = [1, 0]
+    if direction[0] != -1:
+        direction = [1, 0]
 
 
 def left(event):
     global direction
-    direction = [-1, 0]
+    if direction[0] != 1:
+        direction = [-1, 0]
 
 
 def up(event):
     global direction
-    direction = [0, -1]
+    if direction[1] != 1:
+        direction = [0, -1]
 
 
 def down(event):
     global direction
-    direction = [0, 1]
+    if direction[1] != -1:
+        direction = [0, 1]
 
 
 if __name__ == '__main__':
     snake = Serpent([Case(7, 20), Case(7, 21), Case(7, 22)])
-    print(snake)
     direction = [0, -1]
     tk = Tk()
     can = Canvas(tk, width=500, height=500, bg="black")
